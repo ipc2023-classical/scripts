f053627fd1e3b7e6127fc3748fa3162f3d9f46a8
Florian Pommerening <florian.pommerening@unibas.ch>
2023-07-01 22:01:48 +0200

    full size experiment on 10 cores

---
+++ experiments/07-01-full-sized-sat.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from lab.environments import BaselSlurmEnvironment, LocalEnvironment
+
+import benchmarks
+import experiment
+import planners
+import report
+import tracks
+
+TRACK = tracks.SAT
+TIME_LIMIT = 1800 #s
+MEMORY_LIMIT = 8192 #MB
+
+if experiment.running_on_cluster():
+    TESTRUN = False
+    ENVIRONMENT = BaselSlurmEnvironment(
+        partition="infai_3",
+        email="florian.pommerening@unibas.ch",
+        memory_per_cpu=f"{10*3.5*1024}M",  # We reserve enough memory such that each task blocks 10 nodes (actual memory limit is set internally)
+        export=["PATH"],
+    )
+else:
+    TESTRUN = True
+    ENVIRONMENT = LocalEnvironment(processes=2)
+
+exp = experiment.IPCExperiment(track=TRACK, time_limit=TIME_LIMIT, memory_limit=MEMORY_LIMIT, environment=ENVIRONMENT)
+exp.add_planners(planners.get_participating(TRACK))
+exp.add_suite(*benchmarks.get_benchmark_suite(TRACK, TESTRUN))
+
+exp.add_report(report.IPCReport(attributes=report.IPCReport.DEFAULT_ATTRIBUTES), outfile=f"{exp.name}.html")
+for planner in planners.get_participating(TRACK):
+    exp.add_report(report.IPCReport(attributes=report.IPCReport.DEFAULT_ATTRIBUTES, filter_algorithm=planner.shortname), outfile=f"{planner.shortname}.html")
+exp.add_parse_again_step()
+exp.run_steps()
